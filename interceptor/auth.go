8d12c69671343ca438921ea1b2edb1c2e336dddd
K S Anoop <anoop2014.aks@gmail.com>
2023-10-04 21:46:10 +0530

    base64 auth implemented

--- interceptor/auth.go
+++ interceptor/auth.go
@@ -1,13 +1,40 @@
 package interceptor
 
 import (
+	"encoding/base64"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
+
+func validateUser(authTokenString string) bool {
+	var AUTHUSER = os.Getenv("AUTH_USERNAME")
+	var AUTHPASS = os.Getenv("AUTH_PASSWORD")
+	// log.Print(AUTHUSER + ":" + AUTHPASS)
+	if string(authTokenString) == (AUTHUSER + ":" + AUTHPASS) {
+		return true
+	}
+	return false
+}
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		// log.Println("In Auth")
+
+		authInput := r.Header.Get("Authorization")
+		key := strings.Split(authInput, " ")
+
+		decoded, err := base64.StdEncoding.DecodeString(key[1])
+		if err != nil {
+			log.Println("Auth Erorr: error decoding , ", err)
+			http.Error(w, "Error Authenticating", http.StatusInternalServerError)
+		}
+
+		// log.Print(string(decoded))
+		if validateUser(string(decoded)) == false {
+			http.Error(w, "Authentication failed, Wrong credentials ", http.StatusForbidden)
+		}
+
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
 	})
--- main.go
+++ main.go
@@ -13,12 +13,14 @@ import (
 func main() {
 	router := mux.NewRouter()
 
+	router.Use(interceptor.LoggingMiddleware)
+	router.Use(interceptor.AuthMiddleware)
+
 	router.HandleFunc("/index", api.Index).Methods("GET")
+
 	router.HandleFunc("/getAllCustomers", api.GetAllCustomers).Methods("GET")
 	router.HandleFunc("/getCustomer/{id}", api.GetCustomerById).Methods("GET")
 	router.HandleFunc("/addCustomer", api.AddCustomer).Methods("POST")
-	router.Use(interceptor.LoggingMiddleware)
-	router.Use(interceptor.AuthMiddleware)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"http://localhost:4200"},
@@ -27,5 +29,4 @@ func main() {
 
 	log.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", c.Handler(router)))
-	log.Printf("Server Started on port 8080")
 }
